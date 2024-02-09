a806b43ae61515fab5ef306def77aebb62d7b134
Debmalya Sen <debmalyasen37@gmail.com>
2024-02-09 14:35:06 +0530

    updated page.tsx, layout.tsx #fixed the navbar issue, made it directly to layout.tsx for better page handling

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import NavBar from "./NavBar";
 export default function Home() {
   return (
     <div>
-      <NavBar />
       <span className="flex pl-5">tracking your development issues....</span>
     </div>
   );
